b976bb71570611e28b6ab1ada7da73edb6ef95a2
emiliecoudrat <coudrat.emilie@gmail.com>
2014-12-08 19:52:37 +0100

    dernier commit du 8 dec

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,7 +1,6 @@
 class Line < ActiveRecord::Base
   belongs_to :user
   has_many :posts
-  has_many :comments, through: :posts
 
   validates :place, :city, :address, :date, :user_id, presence: true
 end
