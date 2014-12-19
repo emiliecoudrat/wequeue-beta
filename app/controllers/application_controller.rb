752ac3b09bc7aa0adb847174735a271cf4d3173c
emiliecoudrat <coudrat.emilie@gmail.com>
2014-12-19 17:42:07 +0100

    sign in amélioré mais à finir

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:last_name, :first_name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:last_name, :first_name, :email, :password, :password_confirmation, :current_password) }
+  end
+
+  def layout_by_resource
+    if devise_controller? && action_name == "new"
+      "sign"
+    else
+      "application"
+    end
   end
 
 end
