3368bf5c35cc8ed46cc365a42350102836602a27
emiliecoudrat <coudrat.emilie@gmail.com>
2015-01-13 13:51:43 +0100

    autocomplete google résolu

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,25 +1,38 @@
 function initializeAutocomplete(id) {
   var element = document.getElementById(id);
-  if (element) {
-    var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
-    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-  }
+  var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode','establishment'] });
+  google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
 }
 
 google.maps.event.addDomListener(window, 'load', function() {
-  initializeAutocomplete('user_input_autocomplete_address');
+  initializeAutocomplete('user_input_autocomplete_establishment');
 });
 
 function onPlaceChanged() {
   var place = this.getPlace();
+  var street_number = '';
+  var route = '';
+  var locality = '';
+  var postal_code = '';
 
-  for (var i in place.address_components) {
+ for (var i in place.address_components) {
     var component = place.address_components[i];
-    for (var j in component.types) {
-      var type_element = document.getElementById(component.types[j]);
-      if (type_element) {
-        type_element.value = component.long_name;
+    for (var j in component.types) {  // Some types are ["country", "political"]
+       if (component.types[j] == 'street_number') {
+        street_number = component.long_name;
+      } else if (component.types[j] == 'route') {
+        route = component.long_name;
+      } else if (component.types[j] == 'locality') {
+        locality = component.long_name;
+      } else if (component.types[j] == 'postal_code') {
+        postal_code = component.long_name;
       }
     }
   }
+
+$('#user_input_autocomplete_establishment').val(place.name);
+$('#address').val(street_number + ' ' + route);
+$('#postal_code').val(postal_code);
+$('#locality').val(locality);
+
 }
